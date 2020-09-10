c10ae3ab8bea4841c7f44010c3935afb5877c20b
JoisContreras <70292281+JoisContreras@users.noreply.github.com>
2020-09-10 00:20:51 -0500

    Menú con diccionarios en python
    Se agregaron las opciones de Eliminar y Consultar o Buscar.

--- Diccionario/diccionario.py
+++ Diccionario/diccionario.py
@@ -3,6 +3,8 @@ def menu():
     print('.......................')
     print('1. AGREGAR')
     print('2. IMPRIMIR')
+    print('3. ELIMINAR')
+    print('4. CONSULTAR O BUSCAR')
     print('5. SALIR')
     opcion=input('Eliga una de las opciones: ')
     return opcion
@@ -13,17 +15,17 @@ def main():
         
         if opcion=='1':
             print('\n'+'>>>>>AGREGAR')
-            funcionarios={
-                "funcionario1":{ 
+            diccionario={
                 'id':1006569403,
                 'nombre_funcionario':'Yairinis Del Toro',
                 'cargo':'Ingeniera de sistemas',
                 'salario':2000000 
-                },
-            "funcionario2":{'id':"1006569404", 'nombre_funcionario2' :"elis mejia", 'cargo':"secretaria", 'salario' :"1000000" },
-            "funcionario3":{'id':"1006569404", 'nombre_funcionario3' :"luisa marta", 'cargo':"administradora", 'salario' :"1000000" }
-            }
-            print(funcionarios)    
+                }
+            diccionario1={'id':"1006569404", 'nombre_funcionario2' :"elis mejia", 'cargo':"secretaria", 'salario' :"1000000" }
+            
+            print(diccionario,diccionario1)
+            
+            
             
         if opcion=='2':
             print('\n'+'>>>>>> IMPRIMIR >>>>>')
@@ -36,7 +38,39 @@ def main():
             print(diccionario)
             print('')
           
+        if  opcion=='3':
+            print('\n'+'>>>>>> ELIMINAR >>>>>')
+            diccionario={
+                'id':1006569403,
+                'nombre_funcionario':'Yairinis Del Toro',
+                'cargo':'Ingeniera de sistemas',
+                'salario':2000000
+                }
+            diccionario1={'id':"1006569404", 'nombre_funcionario2' :"elis mejia", 'cargo':"secretaria", 'salario' :"1000000" }
+            diccionario2={'id':"1006566583", 'nombre_funcionario3' :"fabiana meza", 'cargo':"docente", 'salario' :"1000000" }
+            diccionario2.clear()
+            print(diccionario, diccionario1, diccionario2)
+            print('EL FUNCONARIO HA SIDO ELIMINADO')
+            
+        if   opcion=='4':
+            print('\n'+'>>>>>> CONSULTAR O BUSCAR >>>>>')
+            diccionario={"funcionario1":{
+                'id':1006569403,
+                'nombre':'Yairinis Del Toro',
+                'cargo':'Ingeniera de sistemas',
+                'salario':2000000}
+            }
+            for key,value in diccionario.items():
+                print(f"{key}:{value}")
+            
         
+            
+            
+            print(diccionario)
+          
+            print('')
+            
+            
         elif opcion=='5':
             print('\n'+'>>>>>> saliendo >>>>>')
             break
